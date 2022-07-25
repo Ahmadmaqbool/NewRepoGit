bfc98d10d21a0feb208111d2ca2aeee5ad4f4cfc
MAHMAD <103309541+Ahmadmaqbool@users.noreply.github.com>
2022-07-25 14:31:51 +0530

    Update SimpleGitBashTest.java

--- src/test/java/com/maven/Project/SimpleGitBashTest.java
+++ src/test/java/com/maven/Project/SimpleGitBashTest.java
@@ -3,5 +3,7 @@ package com.maven.Project;
 public class SimpleGitBashTest {
        public void gitBashTest() {
     	   System.out.println("class is created");
+              //hiii
+ 
        }
 }
