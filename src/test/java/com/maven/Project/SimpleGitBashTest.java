c787087ca190328361a9842895eed501738de934
MAHMAD <103309541+Ahmadmaqbool@users.noreply.github.com>
2022-07-26 12:02:54 +0530

    Update SimpleGitBashTest.java

--- src/test/java/com/maven/Project/SimpleGitBashTest.java
+++ src/test/java/com/maven/Project/SimpleGitBashTest.java
@@ -6,14 +6,9 @@ public class SimpleGitBashTest {
 
     	   System.out.println("hiii");
     	   System.out.println("hello");
-    	 //new changes
+    
     	   System.out.println("my");
-    	   //again new changes.
-    	   //fgfrd
-      //hyy
-              //wow
-         //new tysss
-    	   //we change
+    
 
        }
 }
