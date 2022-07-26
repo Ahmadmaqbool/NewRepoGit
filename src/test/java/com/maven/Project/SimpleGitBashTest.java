497281ae87e3bebdcf572bc46feba5d9a34030e6
Ahmadmaqbool <ahmadmaqbool363@gmail.com>
2022-07-26 10:48:01 +0530

    commited

--- src/test/java/com/maven/Project/SimpleGitBashTest.java
+++ src/test/java/com/maven/Project/SimpleGitBashTest.java
@@ -3,5 +3,10 @@ package com.maven.Project;
 public class SimpleGitBashTest {
        public void gitBashTest() {
     	   System.out.println("class is created");
+    	   System.out.println("hiii");
+    	   System.out.println("hello");
+    	 //new changes
+    	   System.out.println("my");
+    	   //fgfrd
        }
 }
